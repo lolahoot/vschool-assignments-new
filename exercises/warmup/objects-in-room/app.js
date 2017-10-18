8d9d3598e940c769c1fcf46d60263951c04b28f7
Elaine Hardy <laniehardy@gmail.com>
2017-10-18 09:27:34 -0600

    finished warm-up add warmup directory

---
+++ exercises/warmup/objects-in-room/app.js
@@ -0,0 +1,30 @@
+var fireplace {
+  color: "black",
+  mantle type: "wood",
+  surround material: "tile",
+  heat source: "gas"
+}
+
+var coffeTable {
+  heightInInches: 15,
+  lengthInInches: 48,
+  scottsFavorite: true,
+  materials: {
+    surface: "tile",
+    legs: {
+      material: "metal",
+      style: "hairpin"
+    }
+  }
+}
+
+var chair {
+  type: "rocking",
+  color: {
+    legs: "black",
+    arms: "brown",
+    accent: "gold leafing"
+  },
+  material: "wood",
+  heirloom: true
+}
