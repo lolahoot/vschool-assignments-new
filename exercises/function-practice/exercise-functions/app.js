93eb37dcbf71b88c2348a12fa118dd5b995b1562
Elaine Hardy <laniehardy@gmail.com>
2017-10-23 08:59:11 -0600

    If statements, For Loops, CSS Flags

--- exercises/function-practice/exercise-functions/app.js
+++ exercises/function-practice/exercise-functions/app.js
@@ -1,10 +1,18 @@
-function add(a, b) {
-  return a + b;
-}
-console.log(add(7, 14));
+// function add(a, b) {
+//   return a + b;
+// }
+// console.log(add(7, 14));
 
-function largest(a, b, c) {
-  for (i = 0; i < arguments.length; i++) {
-    if (arguments[i] > max)
-  }
+// var largest = function(a, b, c) {
+//     return Math.max(a, b, c);
+// }
+// console.log(largest(5, 1, 15));
+
+// function evenOdd(a) {
+//   return (a & 1) ? "odd" : "even";
+// }
+// console.log(evenOdd(4));
+
+var string = function("a") {
+
 }
