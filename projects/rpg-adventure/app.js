e2e603cd02f3a8275826a28d4fe163136eacbe83
Elaine Hardy <laniehardy@gmail.com>
2017-11-06 12:12:09 -0700

    rpg-adventure, css-zen-garden, matrix-debug

---
+++ projects/rpg-adventure/app.js
@@ -0,0 +1,48 @@
+var ask = require("readline-sync");
+var name = ask.question("Hello, What is your name?")
+console.log("Hi " + name + ", welome to your Colossal Adventure! Choose well and hang onto your head!")
+// var charArr = ["She-Ra Princess of Power", "Human", "Dwarf", "Dragon", "He-Man"]
+var character = ask.keyInSelect(["She-Ra Princess of Power", "Human", "Dwarf", "Dragon", "He-Man"]);
+
+
+console.log("A few tips to make your adventure more awesome. Pressing 'W' will start the game and have your character begin to move. Typing 'print' at anytime will tell you how much HP and each item in your inventory");
+
+function character() {
+
+}
+
+function inventory() {
+
+}
+
+function enemyCreation() {
+
+}
+
+function walk(){
+
+}
+
+function run(){
+
+}
+
+function fight() {
+
+}
+
+function attackEnemy(){
+
+}
+
+function enemyAttack() {
+
+}
+
+function die() {
+
+}
+
+function enemyDie() {
+
+}
